412132b3b6c24024ce6161c0520394718abc75e0
stephen <chanstephen2121@gmail.com>
2022-05-26 21:04:45 -0400

    project page changes

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,35 +1,47 @@
 <template>
   <div class="container">
-    <div class="title"> 1 </div>
-    <div class="title"> 2 </div>
-    <div class="title"> 3 </div>
-    <div class="title"> 4 </div>
-  </div>
-  <div class="container">
-    <div class="box"> 1 </div>
-    <div class="box"> 2 </div>
-    <div class="box"> 3 </div>
-    <div class="box"> 4 </div>
-  </div>
+	<div class="box"> Crypto Portfolio </div>
+	<div class="box2"> Salary Expense Tracker </div>
+	<div class="box"> Fullstack HTML5 Pong</div>
+	<div class="box2"> Weather App </div>
+	<div class="box"> Movie Search </div>
+</div>
 </template>
 
 <style>
 
 .container {
-  width: 100%;
-  height: auto;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around; 
-  flex-flow: wrap;
+		width: 90%;
+		padding: 20px;
+		margin: 100px auto;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+.box2,
+.box {
+	width: 250px;
+	height: 300px;
+	background: #05cee9;
+  margin: 0px 10px;
+  transition: 1s;
+  font-weight: bold;
+  color: black;
+  text-align: center;
+  font-size: 13px;
+  font-family: monospace;
+  font-size: 20px;
+}
+.box:hover {
+	transform: scale(1.3);
+	background: rgba(2, 151, 177, 0.521);
+	z-index: 2;
+  letter-spacing: 3px;
+	box-shadow: 2px 2px 2px #000;
 }
 
-.box {
-  width: 300px;
-  height: 300px;
-  background: yellow;
-  margin: 20px;
-  box-sizing: border-box;
-  font-size: 50px;
+.box2:hover {
+  transform: rotate(360deg);
+  background: rgba(2, 151, 177, 0.521);
 }
 </style>
